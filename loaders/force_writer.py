0d5577061c5be264ce76543872150010a0f259d2
Preda Andrei <preda.andrei174@gmail.com>
2019-10-26 22:50:24 +0300

    Add ForceWriter class

---
+++ loaders/force_writer.py
@@ -0,0 +1,23 @@
+import errno
+import os
+
+from .test_data import TestData
+
+class ForceWriter:
+  def write_tests(self, tests, dest):
+    for (i, test) in enumerate(tests):
+      input_path = os.path.join(dest, f'{i}.in')
+      output_path = os.path.join(dest, f'{i}.out')
+
+      self.create_and_clear(dest)
+      with open(input_path, 'w') as fout:
+        fout.write(test.input_data + '\n\0')
+      with open(output_path, 'w') as fout:
+        fout.write(test.output_data)
+
+  def create_and_clear(self, dest):
+    os.makedirs(dest, exist_ok=True)
+
+    for filename in os.listdir(dest):
+      path = os.path.join(dest, filename)
+      os.remove(path)
